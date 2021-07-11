5a245fdc08721bd62e24dcd07e868cbd43961e21
Elziekio <anthony.deamer@hotmail.com>
2021-07-11 16:38:13 +1000

    Updating Commit

--- Assets/Script.js
+++ Assets/Script.js
@@ -5,22 +5,25 @@ $("#currentDay").text(currentDate.format("dddd MMMM Do YYYY"));
 //color coding the hours by past, future and present//
 var inputBlock =document.querySelectorAll(".time-block-input past future present");
 inputBlock = moment().format("HH")
+console.log(inputBlock);
 
 var currentHour = moment().format("HH")
+//console.log(currentHour);
 
-moment().isbefore();
-document.querySelector(".past");
+$(document.querySelector(".past"));
 
 
-//if (inputBlock) {
-    // Compares row id to current hour and sets color accordingly
-   // if (currentHour === inputBlock) {
-     //   document.querySelector(".present");
-    //} else if ((currentHour < inputBlock) || (currentHour > inputBlock - 6)) {
-       // document.querySelector(".future");
-     // } else if ((currentHour > inputBlock) || (currentHour < inputBlock + 6)) {
-     //   document.querySelector(".past");
-     // } else {
-     //   document.querySelector(".past");
-    //  }
-//};
+if (inputBlock) {
+     //Compares row id to current hour and sets color accordingly
+    if (currentHour === inputBlock) {
+        inputBlock.classList.add("present");
+        inputBlock.classList.remove("future");
+        inputBlock.classList.remove("past");
+    } else if ((currentHour < inputBlock) || (currentHour > inputBlock - 6)) {
+        document.querySelector(".future");
+      } else if ((currentHour > inputBlock) || (currentHour < inputBlock + 6)) {
+        document.querySelector(".past");
+      } else {
+        document.querySelector(".past");
+      }
+};
