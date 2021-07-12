1fbb113be19f699709fb8986bc8e12e1f10a7e13
Elziekio <anthony.deamer@hotmail.com>
2021-07-12 22:51:49 +1000

    updating files commit

--- Assets/Script.js
+++ Assets/Script.js
@@ -3,27 +3,49 @@ var currentDate = moment();
 $("#currentDay").text(currentDate.format("dddd MMMM Do YYYY"));
 
 //color coding the hours by past, future and present//
-var inputBlock =document.querySelectorAll(".time-block-input past future present");
-inputBlock = moment().format("HH")
-console.log(inputBlock);
+var inputBoxes = document.querySelectorAll(".time-block-input");
+var test = moment().format("HH")
 
 var currentHour = moment().format("HH")
-//console.log(currentHour);
 
 $(document.querySelector(".past"));
+$(document.querySelector(".present"));
+$(document.querySelector(".future"));
 
+// Compares row id to current hour and sets color accordingly
+for(var i=0; i < inputBoxes.length; i++) {
+  
+    
+   if (currentHour === inputBoxes[i]) {
+     console.log(inputBoxes[i]);
+       inputBoxes[i].getAttribute('data-time')
+       inputBoxes[i].classList.add("present");
+       inputBoxes[i].classList.remove("future");
+       inputBoxes[i].classList.remove("past");
+   } else if ((currentHour < test) || (currentHour > test - 6)) {
+       inputBoxes[i].getAttribute('data-time')
+       inputBoxes[i].classList.add("future");
+       inputBoxes[i].classList.remove("present");
+       inputBoxes[i].classList.remove("past");
+     } else if ((currentHour > test) || (currentHour < test + 6)) {
+       inputBoxes[i].getAttribute('data-time')
+       inputBoxes[i].classList.add("past");
+       inputBoxes[i].classList.remove("future");
+       inputBoxes[i].classList.remove("present");
+     } else {
+       inputBoxes[i].getAttribute('data-time')
+       inputBoxes[i].classList.add("past");
+       inputBoxes[i].classList.remove("future");
+       inputBoxes[i].classList.remove("present");
+     }
+  
+};
 
-if (inputBlock) {
-     //Compares row id to current hour and sets color accordingly
-    if (currentHour === inputBlock) {
-        inputBlock.classList.add("present");
-        inputBlock.classList.remove("future");
-        inputBlock.classList.remove("past");
-    } else if ((currentHour < inputBlock) || (currentHour > inputBlock - 6)) {
-        document.querySelector(".future");
-      } else if ((currentHour > inputBlock) || (currentHour < inputBlock + 6)) {
-        document.querySelector(".past");
-      } else {
-        document.querySelector(".past");
-      }
+//when button is clicked then the it will save the input text to local storage and parist once the page is refreshed.
+var saveBtn = document.querySelectorAll("saveBtn");
+
+saveBtn.onclick = function(){
+  //var inputValue = inputBoxes.value.trim();
+  console.log(inputValue);
+  localStorage.setItem("Event", inputBoxes.textContent);
 };
